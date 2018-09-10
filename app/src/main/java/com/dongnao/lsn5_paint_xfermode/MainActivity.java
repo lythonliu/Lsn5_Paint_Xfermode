96cc46e287fe2585f7dcecf63fc9f8adf4c4bf15
Lython <liu.junli@moxiangroup.com>
2018-09-10 15:49:08 +0800

    初始化

---
+++ app/src/main/java/com/dongnao/lsn5_paint_xfermode/MainActivity.java
@@ -0,0 +1,17 @@
+package com.dongnao.lsn5_paint_xfermode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends com.lythonliu.LinkAppCompatActivity {
+    @Override
+    public String getAppName(){
+        return BuildConfig.APP_NAME;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
